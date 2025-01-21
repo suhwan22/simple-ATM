30a2a42eabba1b68baccb29dfc27e7a48dedf18b
suhwan <tnghkswkd123@naver.com>
2025-01-21 18:25:08 +0900

    feat: create ATM interface
    - ATM interface 생성
    - insertCard(), enterPIN(), selectAccount(), seeBalance(), deposit(), withdraw() 선언
    
    resolved #1

---
+++ src/ATM.hpp
@@ -0,0 +1,24 @@
+#ifndef ATM_HPP
+#define ATMP_HPP
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ATM {
+	public:
+    virtual ~ATM() {};
+
+    virtual bool insertCard() = 0;
+
+		virtual bool enterPIN(const string& pin) = 0;
+
+    virtual void selectAccount(const string& accountType) = 0;
+
+		virtual long long seeBalance(const string& pin) = 0;
+		virtual void deposit(long long amount) = 0;
+		virtual void withdraw(long long amount) = 0;
+};
+
+#endif
